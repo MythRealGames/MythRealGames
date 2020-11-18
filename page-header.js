633afa8569287ec5f4203c2706a6a5c332a2d91d
MythRealGames <65873243+MythRealGames@users.noreply.github.com>
2020-11-17 22:38:19 -0600

    Add files via upload

---
+++ page-header.js
@@ -0,0 +1,7 @@
+
+document.write('<header id="header" class="alt">'
+	+'<h1><a href="index.html" class="logo"><img src="images/header-logo.png" style="vertical-align: middle; padding-right: 0.5em;"><b id="mythreal">MythReal Games</b></a></h1>'
+	+'<nav>'
+	+'	<a href="#menu">Menu</a>'
+	+'</nav>'
++'</header>');
